f3c04bf382a0270518d322c9a4461f7f71d6267b
Chikane <C51564@launcher.int.abnamro.com>
2018-11-29 13:30:45 +0100

    aws pipeline test 1

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 var time = require('time');
 exports.handler = (event, context, callback) => {
     var currentTime = new time.Date(); 
+    console.log("Hello from Vivek!");
     currentTime.setTimezone("America/Los_Angeles");
     callback(null, {
         statusCode: '200',
